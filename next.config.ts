33b158464d7d8e59ca8ec2489bb67ae3d4ad97e6
Edwin Aguiar <generaextra@gmail.com>
2025-09-11 21:54:39 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -11,16 +11,15 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     cpus: 1,
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
+  },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
       },
     },
   },
-  swcMinify: true,
 };
 
 export default nextConfig;
