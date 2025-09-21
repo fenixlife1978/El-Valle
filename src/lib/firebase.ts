c1b8b9eb96c3771ba951194ab716d69186d32efb
Edwin Aguiar <generaextra@gmail.com>
2025-09-21 20:09:16 +0000

    {   apiKey: "AIzaSyBxKTp_QYuxHWB18jirph5vhY6tWCWR_HI",   authDomain: "co

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,10 +4,10 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2a...Y89fNXq0",
+  apiKey: "AIzaSyBxKTp_QYuxHWB18jirph5vhY6tWCWR_HI",
   authDomain: "condominio-prueba.firebaseapp.com",
   projectId: "condominio-prueba",
-  storageBucket: "condominio-prueba.appspot.com",
+  storageBucket: "condominio-prueba.firebasestorage.app",
   messagingSenderId: "787293423330",
   appId: "1:787293423330:web:7e982a8ecc839f666840bb",
   measurementId: "G-D2YHG41VJJ"
