d480e83b9f7a1d10a92bd2fbaca479ec94380199
Edwin Aguiar <generaextra@gmail.com>
2025-09-29 23:03:33 +0000

    Ya he creado 3l usuario en firebase autenticación y nada, sigue igual. D

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
+
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxKTp_QYuxHWB18jirph5vhY6tWCWR_HI",
-  authDomain: "condominio-prueba.firebaseapp.com",
-  projectId: "condominio-prueba",
-  storageBucket: "condominio-prueba.firebasestorage.app",
-  messagingSenderId: "787293423330",
-  appId: "1:787293423330:web:7e982a8ecc839f666840bb",
-  measurementId: "G-D2YHG41VJJ"
+  apiKey: "AIzaSyA1234567890abcdefghijklmno",
+  authDomain: "studio-dev-project.firebaseapp.com",
+  projectId: "studio-dev-project",
+  storageBucket: "studio-dev-project.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:1234567890abcdef",
+  measurementId: "G-ABCDEFGHIJ"
 };
 
 
