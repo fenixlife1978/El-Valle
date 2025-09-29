14f270b05f14a6624bfd6e7e36995ef115da07ca
Edwin Aguiar <generaextra@gmail.com>
2025-09-29 23:27:19 +0000

    Entiendo. El error auth/api-key-not-valid es muy claro: la clave de API

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {"apiKey":"AIzaSyA1234567890abcdefghijklmno","authDomain":"studio-dev-project.firebaseapp.com","projectId":"studio-dev-project","storageBucket":"studio-dev-project.appspot.com","messagingSenderId":"123456789012","appId":"1:123456789012:web:1234567890abcdef","measurementId":"G-ABCDEFGHIJ"};
+const firebaseConfig = {"apiKey":"AIzaSyD_2vA_v43r5v-1234567890","authDomain":"condo-connect-dev.firebaseapp.com","projectId":"condo-connect-dev","storageBucket":"condo-connect-dev.appspot.com","messagingSenderId":"123456789012","appId":"1:123456789012:web:1234567890abcdef","measurementId":"G-1234567890"};
 
 
 // Initialize Firebase
@@ -14,3 +14,4 @@ const storage = getStorage(app);
 const auth = getAuth(app);
 
 export { app, db, storage, auth };
+
