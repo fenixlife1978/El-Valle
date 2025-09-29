78b8bac78d231f426179b75ee53b36a1a5c9bb71
Edwin Aguiar <generaextra@gmail.com>
2025-09-29 23:24:25 +0000

    pero no se perderan mis datos actuales de la app, seguira funcionando ig

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,15 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1234567890abcdefghijklmno",
-  authDomain: "studio-dev-project.firebaseapp.com",
-  projectId: "studio-dev-project",
-  storageBucket: "studio-dev-project.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:1234567890abcdef",
-  measurementId: "G-ABCDEFGHIJ"
-};
+const firebaseConfig = {"apiKey":"AIzaSyA1234567890abcdefghijklmno","authDomain":"studio-dev-project.firebaseapp.com","projectId":"studio-dev-project","storageBucket":"studio-dev-project.appspot.com","messagingSenderId":"123456789012","appId":"1:123456789012:web:1234567890abcdef","measurementId":"G-ABCDEFGHIJ"};
 
 
 // Initialize Firebase
