7e5f6172f15fef771d7bb838c1e821500ab08204
Edwin Aguiar <generaextra@gmail.com>
2025-09-30 01:38:51 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/page f

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: [
-        '3000-firebase-studio-1755127519376.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
-      ]
+      
     }
   }
 }
