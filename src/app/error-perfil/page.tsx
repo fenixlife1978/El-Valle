8df8095f3feb89050f442b2687ed899192376021
Edwin Aguiar <generaextra@gmail.com>
2025-10-10 02:19:06 +0000

    Try fixing this error: `Runtime ReferenceError: CardFooter is not define

--- src/app/error-perfil/page.tsx
+++ src/app/error-perfil/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { AlertTriangle } from 'lucide-react';
 import Link from 'next/link';
 
