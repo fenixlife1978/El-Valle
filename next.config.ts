42a0a44e2c023e7a06c8b7585368d8f4bcf521f2
Edwin Aguiar <generaextra@gmail.com>
2025-11-04 19:37:06 +0000

    Corrige la advertencia de Next.js sobre "Cross origin request detected"

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {},
+    allowedDevOrigins: [
+      'https://3001-firebase-studio-1755127519376.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
+    ]
   },
 };
 
