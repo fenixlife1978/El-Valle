fdd03f6328941f23c8488978866edbb2e1ca9ffa
Edwin Aguiar <generaextra@gmail.com>
2025-11-04 19:49:15 +0000

    Corrige el archivo `next.config.ts` para que Next.js 15.5.4 no muestre a

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    'https://3002-firebase-studio-1755127519376.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
+    'https://3003-firebase-studio-1755127519376.cluster-joak5ukfbnbyqspg4tewa33d24.cloudworkstations.dev'
   ],
   experimental: {
-    serverActions: true,
+    serverActions: {},
   },
 };
 
