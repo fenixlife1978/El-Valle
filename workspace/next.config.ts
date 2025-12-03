a7ce2722ab2c0ca7e28dcd55b4ac58f048db6ae0
Edwin Aguiar <generaextra@gmail.com>
2025-12-03 18:50:44 +0000

    Try fixing this error: `Runtime Error: ENOENT: no such file or directory

--- workspace/next.config.ts
+++
@@ -1,9 +0,0 @@
-
-const nextConfig = {
-  experimental: {
-    serverActions: {},
-    allowedDevOrigins: process.env.DEV_ORIGIN ? [process.env.DEV_ORIGIN] : [],
-  },
-} as const;
-
-export default nextConfig;
