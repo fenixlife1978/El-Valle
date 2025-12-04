4ed2eb016568591fe407d532d8e5cf74ad4ffe20
Edwin Aguiar <generaextra@gmail.com>
2025-12-04 18:45:33 +0000

    Quiero que restaures mi app completamente al punto exacto de funcionamie

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -38,8 +38,8 @@ function WelcomePageContent() {
         {loading ? (
           <Skeleton className="w-24 h-24 rounded-full mx-auto mb-6" />
         ) : (
-          <Avatar className="w-24 h-24 mx-auto mb-6 bg-white p-1">
-             <AvatarImage src={logoUrl || ''} alt="Company Logo" className="object-contain" />
+          <Avatar className="w-24 h-24 mx-auto mb-6">
+             <AvatarImage src={logoUrl || ''} alt="Company Logo" />
              <AvatarFallback>VC</AvatarFallback>
           </Avatar>
         )}
