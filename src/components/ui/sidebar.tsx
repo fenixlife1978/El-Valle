5a904dbae17a1ec2f5c22f455ada339cb5b5c057
Edwin Aguiar <generaextra@gmail.com>
2026-01-11 23:07:21 +0000

    quiero que el icono de menu sea mas grande  y se muestre la palabra "Men

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -271,15 +271,15 @@ const SidebarTrigger = React.forwardRef<
       ref={ref}
       data-sidebar="trigger"
       variant="ghost"
-      size="icon"
-      className={cn("h-7 w-7", className)}
+      className={cn("h-10 px-3", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
       }}
       {...props}
     >
-      <PanelLeftClose />
+      <span>Menu</span>
+      <PanelLeftClose className="h-5 w-5" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
